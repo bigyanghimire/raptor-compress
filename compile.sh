f0fc738eba0b5258fed1df47d0ae48a7e367bcf9
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-04-16 12:16:07 -0400

    compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
+    $mode=$1
     #cmake -B build -Werror=dev --warn-uninitialized  -DWITH_MPI=ON && \
-    cmake -B build -DWITH_MPI=ON && \
+    cmake -B build -DWITH_MPI=ON -DCLUSTER_MODE=$1 && \
     cmake --build build --parallel $(nproc) -- VERBOSE=1 &&
     clear && \
    mpirun -n 5  build/examples/example
