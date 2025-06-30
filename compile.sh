ed60813ac37043d7add48ea9e25b44eb3e2430dd
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-06-30 10:01:37 -0400

    changes

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -DWITH_MPI=ON && \
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -DWITH_MPI=ON -DWITH_DOXYGEN=OFF && \
 cmake --build build --parallel $(nproc) &&
 clear && \
 mpirun -n 2  build/examples/benchmark_amg
