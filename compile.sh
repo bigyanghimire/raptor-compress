e252cf51e8d10f50b96b0a2823e0845e21636bda
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-03 14:14:21 -0400

    changes

--- compile.sh
+++ compile.sh
@@ -2,9 +2,8 @@ clear
 cmake -B build -DCMAKE_BUILD_TYPE=Debug -DWITH_MPI=ON -DWITH_DOXYGEN=OFF && \
 cmake --build build --parallel $(nproc) &&
 clear && \
-mpirun -n 2  build/examples/benchmark_amg
-
-
+# mpirun -n 2  build/examples/benchmark_amg
+mpirun -n 2 build/examples/benchmark_amg | grep -c 'MPI Send'
 # cd build
 # cmake .. -DWITH_MPI=ON  # Only if you changed config
 # cmake --build . --parallel $(nproc)
--- raptor/core/mpi_types.cpp
+++ raptor/core/mpi_types.cpp
@@ -169,7 +169,7 @@ int RAPtor_MPI_Isend(const void *buf, int count, RAPtor_MPI_Datatype datatype, i
     RAPtor_MPI_Comm_rank(RAPtor_MPI_COMM_WORLD, &rank);
     RAPtor_MPI_Comm_size(RAPtor_MPI_COMM_WORLD, &num_procs);
  
-    std::cout<<"Called >>>>>>>>"<<datatype<<std::endl;
+    std::cout<<"MPI Send"<<std::endl;
     // print_stacktrace();
     if (profile) p2p_t -= RAPtor_MPI_Wtime();
     int val = MPI_Isend(buf, count, datatype, dest, tag, comm, request);
