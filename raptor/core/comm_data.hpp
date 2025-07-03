49167e9be00f6e8338ca02651372558d487ca5a7
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-03 13:43:48 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -64,7 +64,20 @@ public:
     virtual ~CommData()
     {
     };
+void printVector(std::vector<int>& off_proc_column_map, char *disp) {
+    std::cout << disp;
+    for (int val : off_proc_column_map) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
 
+template <typename T>
+void print_values(const T* values, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << "values[" << i << "] = " << values[i] << std::endl;
+    }
+}
     virtual void add_msg(int proc, int msg_size, int* msg_indices = NULL) = 0;
 
     void finalize()
@@ -412,20 +425,7 @@ public:
     {
         send(values, key, mpi_comm, states, compare_func, n_send_ptr, block_size);
     }        
-void printVector(std::vector<int>& off_proc_column_map, char *disp) {
-    std::cout << disp;
-    for (int val : off_proc_column_map) {
-        std::cout << val << " ";
-    }
-    std::cout << std::endl;
-}
 
-template <typename T>
-void print_values(const T* values, size_t size) {
-    for (size_t i = 0; i < size; ++i) {
-        std::cout << "values[" << i << "] = " << values[i] << std::endl;
-    }
-}
     template <typename T>
     void send(const T* values, int key, RAPtor_MPI_Comm mpi_comm, const int block_size = 1,
             std::function<T(T, T)> init_result_func = &sum_func<T, T>,
