525374a163be94ec16ac3cd03718aedc49b6dbe8
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-04 15:03:38 -0400

    changes

--- raptor/gallery/par_matrix_market.cpp
+++ raptor/gallery/par_matrix_market.cpp
@@ -136,20 +136,20 @@ ParCSRMatrix* read_par_mm(const char *fname)
     int rank, num_procs;
     RAPtor_MPI_Comm_rank(RAPtor_MPI_COMM_WORLD, &rank);
     RAPtor_MPI_Comm_size(RAPtor_MPI_COMM_WORLD, &num_procs);
-    // if (rank == 0)
-    // {
-    //              std::cout<<"The number of off oprocess num cols for rank 0 is"<<A->on_proc->nnz<<std::endl;;
-
-    //         printVector(A->off_proc->idx1, "vals rows", A->off_proc->nnz);
-    //         printVector(A->off_proc->idx2, "vals cols", A->off_proc->nnz); // idx1 = { 0 1 2 }
-
-    //     }
-        if (rank == 0)
+    if (rank == 0)
     {
-                 std::cout<<"The number of off oprocess num cols for rank 0 is"<<A->off_proc_num_cols<<std::endl;;
+                //  std::cout<<"The number of off oprocess num cols for rank 0 is"<<A->on_proc->nnz<<std::endl;;
 
+            printVector(A->on_proc->idx1, "vals rows", A->on_proc->nnz);
+            printVector(A->on_proc->idx2, "vals cols", A->on_proc->nnz); // idx1 = { 0 1 2 }
 
         }
+    //     if (rank == 0)
+    // {
+    //              std::cout<<"The number of off oprocess num cols for rank 0 is"<<A->off_proc_num_cols<<std::endl;;
+
+
+    //     }
     A->finalize();
 //     std::cout<<"The number of nnzs after is"<<A->off_proc->nnz;
 //    if (rank == 0)
