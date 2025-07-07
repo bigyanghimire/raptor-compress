5686acf9f1c5976ca7c19168f5fbf837586ee944
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-07 13:26:40 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -866,9 +866,9 @@ public:
             std::cout << "compress data size is" << cmpSize<< std::endl;
 
             std::cout << "buffer size" << (end - start) * block_size << std::endl;
-            // std::vector<T> dec_data(datasize);
-            // auto dec_data_p = dec_data.data();
-            // decompress_data(&buf[start * block_size], datasize, cmpData, dec_data_p);
+            std::vector<T> dec_data(datasize);
+            auto dec_data_p = dec_data.data();
+            decompress_data(&buf[start * block_size], datasize, cmpData, dec_data_p);
             RAPtor_MPI_Isend(&(buf[start * block_size]), (end - start) * block_size,
                              datatype, proc, key, mpi_comm, &(requests[i]));
         }
