874891a2e412f2ae2ab2133b105f3483f88068dd
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-14 15:57:19 -0400

    changes

--- raptor/core/sz3_wrapper.cpp
+++ raptor/core/sz3_wrapper.cpp
@@ -25,9 +25,9 @@ namespace raptor
         // return cmpData;
     }
 
-    template char *compress_data<float>(float *uncompressedData, size_t size, size_t cmpSize);
-    template char *compress_data<double>(double *uncompressedData, size_t size, size_t cmpSize);
-    template char *compress_data<int>(int *uncompressedData, size_t size, size_t cmpSize);
+    template char *compress_data<float>(float *uncompressedData, size_t size, size_t& cmpSize);
+    template char *compress_data<double>(double *uncompressedData, size_t size, size_t& cmpSize);
+    template char *compress_data<int>(int *uncompressedData, size_t size, size_t& cmpSize);
 
     template void decompress_data<float>(float *uncompressedData, size_t size, size_t cmpSize, char *cmpData,  float *dec_data_p);
     template void decompress_data<double>(double *uncompressedData, size_t size, size_t cmpSize, char *cmpData , double *dec_data_p);
