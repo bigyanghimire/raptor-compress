f61ea1bd0b43ddd1fc32b0e95e087f821ae9d94f
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-14 14:48:37 -0400

    changes

--- examples/benchmark_amg.cpp
+++ examples/benchmark_amg.cpp
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
     else if (system == 3)
     {
         // const char* file = "/home/bigyang/mtx_data/ex9/ex9.mtx";
-                const char* file = "/home/bigyang/mtx_data/ex9/ex9.mtx";
+                const char* file = "/home/bigyang/mtx_data/boneS10/boneS10.mtx";
 
         A = read_par_mm(file);
     }
