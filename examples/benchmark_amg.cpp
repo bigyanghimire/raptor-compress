1211cc907a9d8341ca7c204e4a8e6f5f10a2faf1
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-14 15:33:30 -0400

    changes

--- examples/benchmark_amg.cpp
+++ examples/benchmark_amg.cpp
@@ -136,9 +136,8 @@ int main(int argc, char* argv[])
 #endif
     else if (system == 3)
     {
-        const char* file = "examples/LFAT5000.mtx";
-                // const char* file = "/home/bigyang/mtx_data/shallow_water1/shallow_water1.mtx";
-
+        // const char* file = "examples/LFAT5000.mtx";
+        const char* file = "/home/bigyang/mtx_data/ex9/ex9.mtx";
         A = read_par_mm(file);
     }
 
