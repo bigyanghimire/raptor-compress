d4c383a5bd77a1a7ee034ce46137f3a14f410dcf
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-14 15:39:05 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -862,15 +862,15 @@ public:
             /*
             * Compression and Decompression
             */
-            // size_t cmpSize;
-            // size_t datasize=(end - start) * block_size;
-            // auto cmpData = compress_data(&buf[start * block_size], datasize, cmpSize);
-            // std::cout << "compress data size is" << cmpSize<< std::endl;
+            size_t cmpSize;
+            size_t datasize=(end - start) * block_size;
+            auto cmpData = compress_data(&buf[start * block_size], datasize, cmpSize);
+            std::cout << "compress data size is" << cmpSize<< std::endl;
 
-            //  std::cout << "buffer size" << (end - start) * block_size << std::endl;
-            // std::vector<T> dec_data(datasize);
-            // auto dec_data_p = dec_data.data();
-            // decompress_data(&buf[start * block_size], datasize, cmpSize, cmpData, dec_data_p);
+             std::cout << "buffer size" << (end - start) * block_size << std::endl;
+            std::vector<T> dec_data(datasize);
+            auto dec_data_p = dec_data.data();
+            decompress_data(&buf[start * block_size], datasize, cmpSize, cmpData, dec_data_p);
             /*
             * Compression and Decompression ends
             */
