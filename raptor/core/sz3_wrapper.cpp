7b24c520e5e07dcd8a4d2dc3641623556552163e
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-15 14:00:40 -0400

    changes

--- raptor/core/sz3_wrapper.cpp
+++ raptor/core/sz3_wrapper.cpp
@@ -12,7 +12,7 @@ namespace raptor
         size_t cmpSizedum = cmpDatadum.size();
         std::cout << "COnf num is" << conf.num << std::endl;
         size_t bufferLen = SZ3::SZ_compress_size_bound<T>(conf);
-        std::cout << "Bufferlens size" << bufferLen <<cmpSizedum<< std::endl;
+        std::cout << "Bufferlens size" << bufferLen <<"cmp size"<<cmpSizedum<< std::endl;
         conf.cmprAlgo = SZ3::ALGO_INTERP_LORENZO;
         conf.errorBoundMode = SZ3::EB_ABS;
         conf.absErrorBound = 1e-3;
