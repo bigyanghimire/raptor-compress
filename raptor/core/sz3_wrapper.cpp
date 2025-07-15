e7d78069cb801adb7395be2bca7b4e8475757a14
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-15 13:53:51 -0400

    changes

--- raptor/core/sz3_wrapper.cpp
+++ raptor/core/sz3_wrapper.cpp
@@ -5,12 +5,14 @@
 namespace raptor
 {
     template <typename T>
-    char *compress_data(T *uncompressedData, size_t size, size_t& cmpSize)
+    char *compress_data(T *uncompressedData, size_t size, size_t &cmpSize)
     {
         SZ3::Config conf({size});
-        std::cout<<"COnf num is"<<conf.num<<std::endl;
+        std::vector<char> cmpDatadum(conf.num);
+        size_t cmpSizedum = cmpDatadum.size();
+        std::cout << "COnf num is" << conf.num << std::endl;
         size_t bufferLen = SZ3::SZ_compress_size_bound<T>(conf);
-        std::cout<<"Bufferlens size"<<bufferLen<<std::endl;
+        std::cout << "Bufferlens size" << bufferLen <<cmpSizedum<< std::endl;
         conf.cmprAlgo = SZ3::ALGO_INTERP_LORENZO;
         conf.errorBoundMode = SZ3::EB_ABS;
         conf.absErrorBound = 1e-3;
@@ -29,12 +31,12 @@ namespace raptor
         // return cmpData;
     }
 
-    template char *compress_data<float>(float *uncompressedData, size_t size, size_t& cmpSize);
-    template char *compress_data<double>(double *uncompressedData, size_t size, size_t& cmpSize);
-    template char *compress_data<int>(int *uncompressedData, size_t size, size_t& cmpSize);
+    template char *compress_data<float>(float *uncompressedData, size_t size, size_t &cmpSize);
+    template char *compress_data<double>(double *uncompressedData, size_t size, size_t &cmpSize);
+    template char *compress_data<int>(int *uncompressedData, size_t size, size_t &cmpSize);
 
-    template void decompress_data<float>(float *uncompressedData, size_t size, size_t cmpSize, char *cmpData,  float *dec_data_p);
-    template void decompress_data<double>(double *uncompressedData, size_t size, size_t cmpSize, char *cmpData , double *dec_data_p);
+    template void decompress_data<float>(float *uncompressedData, size_t size, size_t cmpSize, char *cmpData, float *dec_data_p);
+    template void decompress_data<double>(double *uncompressedData, size_t size, size_t cmpSize, char *cmpData, double *dec_data_p);
     template void decompress_data<int>(int *uncompressedData, size_t size, size_t cmpSize, char *cmpData, int *dec_data_p);
 
 }
