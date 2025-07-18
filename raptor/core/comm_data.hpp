33cb4f22a64230d5afd44c4175e7982e53ed0fdf
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-18 13:11:37 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -905,7 +905,7 @@ bool is_all_zero(const T* data, size_t size) {
             //     wait_for_pid();
             // }
             size_t datasize = (end - start) * block_size;
-            if (datasize!=41)
+            if (datasize>50)
             {
             int total_elements = datasize;
              int offset = start * block_size;
