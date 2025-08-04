1daa942ab6876517b30392b73a214737db8e72a3
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-04 14:23:13 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -934,11 +934,10 @@ bool is_all_zero(const T* data, size_t size) {
             
             // print_values(dec_data_p,datasize,"before values");
             // decompress_data(input_data.data(), datasize, cmpSize, cmpData, dec_data_p);
-            //  RAPtor_MPI_Isend(dec_data.data(), (end - start) * block_size,
-            //                   datatype, proc, key, mpi_comm, &(requests[i]));
-          
-                              RAPtor_MPI_Isend(&(buf[start * block_size]), (end - start) * block_size,
-                                           datatype, proc, key, mpi_comm, &(requests[i]));
+             RAPtor_MPI_Isend(dec_data.data(), (end - start) * block_size,
+                              datatype, proc, key, mpi_comm, &(requests[i]));
+        // RAPtor_MPI_Isend(&(buf[start * block_size]), (end - start) * block_size,
+        //                                    datatype, proc, key, mpi_comm, &(requests[i]));
 
         }
     }
