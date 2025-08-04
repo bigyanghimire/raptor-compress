1ff8551fcaf66dc04c0d1fb053cfb1ca9341dfb7
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-04 14:34:53 -0400

    changes

--- examples/example.cpp
+++ examples/example.cpp
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
     // Solve Raptor Hierarchy
     MPI_Barrier(MPI_COMM_WORLD);
     time_base = MPI_Wtime();
-    compression_on=1;
+    compression_on=81;
     std::cout<<"Compression on is"<<compression_on<<std::endl;
     int total_iter=ml->solve(x, b);
     compression_on=81;
