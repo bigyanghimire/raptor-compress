a8a33420fff8406121d4158caa8f9efb8c39a856
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-04 16:48:53 -0400

    changes

--- raptor/multilevel/par_multilevel.hpp
+++ raptor/multilevel/par_multilevel.hpp
@@ -517,7 +517,7 @@ namespace raptor
                     {
                         init_profile();
                     }
-
+                    ext_solve_tol=r_norm;
                     iter++;
                     levels[0]->A->residual(sol, rhs, resid);
                     if (fabs(b_norm) > zero_tol)
