5cd2d43558131d049b2a8147ae9812a0dbaf09c3
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-07 15:10:33 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -951,12 +951,13 @@ bool is_all_zero(const T* data, size_t size) {
             */
             // RAPtor_MPI_Isend(cmpData, cmpSize,
             //                   datatype, proc, key, mpi_comm, &(requests[i]));
-            int total_buffer_size=cmpSize+4;
+            int cmp_size=static_cast<int>(cmpSize);
+            int total_buffer_size=cmp_size+4;
             char buff[total_buffer_size];
             int position=0;
-            MPI_Pack(&cmpSize, 1, MPI_INT, buff, total_buffer_size, &position, MPI_COMM_WORLD);
-            MPI_Pack(cmpData, cmpSize, MPI_CHAR, buff, total_buffer_size, &position, MPI_COMM_WORLD);
-            std::cout<<"cmp size was"<<cmpSize<<"position:"<<position<<"total_buffer_size: "<<total_buffer_size<<std::endl;
+            MPI_Pack(&cmp_size, 1, MPI_INT, buff, total_buffer_size, &position, MPI_COMM_WORLD);
+            MPI_Pack(cmpData, cmp_size, MPI_CHAR, buff, total_buffer_size, &position, MPI_COMM_WORLD);
+            std::cout<<"cmp size was"<<cmp_size<<"position:"<<position<<"total_buffer_size: "<<total_buffer_size<<std::endl;
             // RAPtor_MPI_Isend(dec_data, (end - start) * block_size,
             //       datatype, proc, 9999123, mpi_comm, &(requests[i]));
             RAPtor_MPI_Isend(buff, position,
