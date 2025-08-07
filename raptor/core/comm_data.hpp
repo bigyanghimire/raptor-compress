c05e709e04d52da3228bc77b5bad0a7baf67da15
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-07 15:26:56 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -179,7 +179,7 @@ void print_values(const T* values, size_t size, char * disp) {
                 std::cout<<"recv Message size probe is"<<msg_size_probe<<std::endl;
                 char recv_buff[msg_size_probe];
                 std::cout << "Here comes the status" << std::endl;
-                char* cmp_data[msg_size_probe-4];
+                char* cmp_data[msg_size_probe-sizeof(int)];
                 RAPtor_MPI_Irecv(recv_buff, msg_size_probe, MPI_PACKED,
                                  proc, 9999123, mpi_comm, &(requests[i]));
                 MPI_Unpack(recv_buff, msg_size_probe, &recv_position, &cmp_size, 1, MPI_INT, MPI_COMM_WORLD);
@@ -952,7 +952,7 @@ bool is_all_zero(const T* data, size_t size) {
             // RAPtor_MPI_Isend(cmpData, cmpSize,
             //                   datatype, proc, key, mpi_comm, &(requests[i]));
             int cmp_size=static_cast<int>(cmpSize);
-            int total_buffer_size=cmp_size+4;
+            int total_buffer_size=cmp_size+sizeof(int);
             char buff[total_buffer_size];
             int position=0;
             MPI_Pack(&cmp_size, 1, MPI_INT, buff, total_buffer_size, &position, MPI_COMM_WORLD);
@@ -961,10 +961,10 @@ bool is_all_zero(const T* data, size_t size) {
            std::cout << "cmp size was" << cmp_size << "position:" << position << "total_buffer_size: " << total_buffer_size << std::endl;
            // RAPtor_MPI_Isend(dec_data, (end - start) * block_size,
            //       datatype, proc, 9999123, mpi_comm, &(requests[i]));
-          int unpacked_val;
-          int new_position = 0;
-          MPI_Unpack(buff, 1000, &new_position, &unpacked_val, 1, MPI_INT, MPI_COMM_WORLD);
-           std::cout << "Unpacked cmp size after is" << unpacked_val << std::endl;
+        //   int unpacked_val;
+        //   int new_position = 0;
+        //   MPI_Unpack(buff, 1000, &new_position, &unpacked_val, 1, MPI_INT, MPI_COMM_WORLD);
+           std::cout << "Position: " << position << std::endl;
 
           
            RAPtor_MPI_Isend(buff, position,
