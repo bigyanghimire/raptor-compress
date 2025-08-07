d55faeb859e852353575b4df36b19756d8ab14d3
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-07 15:07:07 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -183,6 +183,7 @@ void print_values(const T* values, size_t size, char * disp) {
                 RAPtor_MPI_Irecv(recv_buff, msg_size_probe, MPI_PACKED,
                                  proc, 9999123, mpi_comm, &(requests[i]));
                 MPI_Unpack(recv_buff, msg_size_probe, &recv_position, &cmp_size, 1, MPI_INT, MPI_COMM_WORLD);
+                std::cout<<"Recv cmp size"<<cmp_size<<std::endl;
                 MPI_Unpack(recv_buff, msg_size_probe, &recv_position, cmp_data, cmp_size, MPI_CHAR, MPI_COMM_WORLD);
                 // std::cout<<"on recv side cmp size was"<<cmp_size<<"position:"<<recv_position<<"total_buffer_size: "<<msg_size_probe<<std::endl;
                 T* dec_data=decompress_data<T>((end - start) * block_size, cmp_size, cmp_data[0]);
