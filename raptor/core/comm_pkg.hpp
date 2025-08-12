f66ca82264b4116e2037320c076ff3fc35054394
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-12 15:32:03 -0400

    changes

--- raptor/core/comm_pkg.hpp
+++ raptor/core/comm_pkg.hpp
@@ -687,7 +687,7 @@ void printVector(const std::vector<int>& off_proc_column_map, char *disp) {
                     std::vector<char> cmp_data(cmp_size);
                     MPI_Unpack(tmp_cmp_buf[i].data(), tmp_cmp_buf[i].size(), &recv_position, cmp_data.data(), cmp_size, MPI_CHAR, MPI_COMM_WORLD);
                     T* dec_data=decompress_data<T>(recv_data_size, cmp_size, cmp_data.data());
-                    newbuf.insert(buf.end(), dec_data, dec_data + recv_data_size);
+                    newbuf.insert(newbuf.end(), dec_data, dec_data + recv_data_size);
                 }
                 return newbuf;
                 // std::vector<T>& buf = recv_data->get_buffer<T>();
