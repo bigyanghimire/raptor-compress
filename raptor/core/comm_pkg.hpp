1e12b0521dbfcb054136605d5e7e44b0d1468b8b
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-12 13:48:57 -0400

    changes

--- raptor/core/comm_pkg.hpp
+++ raptor/core/comm_pkg.hpp
@@ -675,9 +675,9 @@ void printVector(const std::vector<int>& off_proc_column_map, char *disp) {
                     int recv_position=0;
                     int cmp_size;
                     char* cmp_data[tmp_cmp_buf.size()-sizeof(int)];
-                    MPI_Unpack(tmp_cmp_buf[i], tmp_cmp_buf[i].size(), &recv_position, &cmp_size, 1, MPI_INT, MPI_COMM_WORLD);
+                    MPI_Unpack(tmp_cmp_buf[i].data(), tmp_cmp_buf[i].size(), &recv_position, &cmp_size, 1, MPI_INT, MPI_COMM_WORLD);
                     std::cout<<"Recv cmp size"<<cmp_size<<std::endl;
-                    MPI_Unpack(tmp_cmp_buf[i], tmp_cmp_buf[i].size(), &recv_position, cmp_data, cmp_size, MPI_CHAR, MPI_COMM_WORLD);
+                    MPI_Unpack(tmp_cmp_buf[i].data(), tmp_cmp_buf[i].size(), &recv_position, cmp_data, cmp_size, MPI_CHAR, MPI_COMM_WORLD);
                 }
                 // std::vector<T>& buf = recv_data->get_buffer<T>();
             }
