13db4bf99c69ef06ebabddac980f2cd26cc2bcd7
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-12 15:31:08 -0400

    changes

--- raptor/core/comm_pkg.hpp
+++ raptor/core/comm_pkg.hpp
@@ -668,6 +668,7 @@ void printVector(const std::vector<int>& off_proc_column_map, char *disp) {
             key++;
             
             std::vector<T>& buf = recv_data->get_buffer<T>();
+            std::vector<T> newbuf;
             //std::vector<T> newbuf;
             // Extract packed data to appropriate buffer
             if(compression_on==1){
@@ -686,15 +687,17 @@ void printVector(const std::vector<int>& off_proc_column_map, char *disp) {
                     std::vector<char> cmp_data(cmp_size);
                     MPI_Unpack(tmp_cmp_buf[i].data(), tmp_cmp_buf[i].size(), &recv_position, cmp_data.data(), cmp_size, MPI_CHAR, MPI_COMM_WORLD);
                     T* dec_data=decompress_data<T>(recv_data_size, cmp_size, cmp_data.data());
-                    buf.insert(buf.end(), dec_data, dec_data + recv_data_size);
+                    newbuf.insert(buf.end(), dec_data, dec_data + recv_data_size);
                 }
+                return newbuf;
                 // std::vector<T>& buf = recv_data->get_buffer<T>();
             }
-            // else{
-            //     std::vector<T>& buf = recv_data->get_buffer<T>();
-            // }
+            else{
+                std::vector<T>& buf = recv_data->get_buffer<T>();
+                 return buf;
+            }
 
-            return buf;
+           
         }
 
         // Transpose Communication
