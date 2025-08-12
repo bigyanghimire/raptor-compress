063164be3a3ee90e502f958d0ec838d8ebea3f9b
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-12 14:07:07 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -166,8 +166,10 @@ void print_values(const T* values, size_t size, char * disp) {
         int size = size_msgs * block_size;
         RAPtor_MPI_Datatype datatype = get_type<T>();
         std::vector<T>& buf = get_buffer<T>();
-        if ((int) buf.size() < size) buf.resize(size);
         std::vector<std::vector<char>>& tmp_cmp_buf = get_cmp_buffer();
+        if ((int) buf.size() < size) buf.resize(size);
+        if ((int) tmp_cmp_buf.size() < size) tmp_cmp_buf.resize(size);
+
         std::cout<<"NUm messages is"<<num_msgs<<std::endl;
         for (int i = 0; i < num_msgs; i++)
         {
