ead204fa8c7828fee5274fcd42a27386b66d750b
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-12 13:59:36 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -189,6 +189,8 @@ void print_values(const T* values, size_t size, char * disp) {
                 tmp_cmp_buf[i].resize(msg_size_probe);
                 RAPtor_MPI_Irecv(tmp_cmp_buf[i].data(), msg_size_probe, MPI_PACKED,
                                  proc, 9999123, mpi_comm, &(requests[i]));
+                MPI_Unpack(tmp_cmp_buf[i].data(), tmp_cmp_buf[i].size(), &recv_position, &cmp_size, 1, MPI_INT, MPI_COMM_WORLD);
+                std::cout<<"comm data recv cmp size"<<cmp_size<<std::endl;
                 // for(int i=0;i<10;i++){
                 //     std::cout<<"Msg size buffer is comm data"<<msg_size_buf[i]<<std::endl;
                 // }
