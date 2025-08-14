027ab558b8acc4ac57eb7843cecf05e8f3219f0a
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-14 12:44:58 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -939,7 +939,6 @@ bool is_all_zero(const T* data, size_t size) {
             // }
             if(compression_on==1){
                  size_t datasize = (end - start) * block_size;
-                 std::cout<<"orig data size is"<<datasize<<std::endl;
                 size_t cmpSize;
                 char *cmpData = compress_data<T>(&buf[start * block_size], datasize, cmpSize);
                 T* dec_data=decompress_data<T>( datasize, cmpSize, cmpData);
@@ -965,6 +964,8 @@ bool is_all_zero(const T* data, size_t size) {
             int cmp_size=static_cast<int>(cmpSize);
             // int cmp_size=10;
             int data_size=static_cast<int>(data_size);
+            std::cout<<"orig data size is"<<datasize<<"and new is"<<data_size<<std::endl;
+
             // int total_buffer_size=cmp_size+sizeof(int)+sizeof(int);
             
             int total_buffer_size=data_size+sizeof(int)+sizeof(int);
