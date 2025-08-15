d11f857c7ec20247bbe5c24fde0d69ad7330b330
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-15 10:00:13 -0400

    changes

---
+++ runjob.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --job-name=amg_rank_0
+#SBATCH --nodes=2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32G
+#SBATCH --time=2:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
