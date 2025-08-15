cfb5bf5721ba93def6a629c5d06f308975e14737
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-15 09:22:24 -0400

    changes

--- raptor/core/comm_data.hpp
+++ raptor/core/comm_data.hpp
@@ -914,8 +914,22 @@ bool is_all_zero(const T* data, size_t size) {
                 // }
                 size_t original_bytes = datasize * sizeof(T);  // actual size in bytes
                 double ratio = static_cast<double>(original_bytes) / cmpSize;
-                std::cout<<"size of cmpdata is"<<sizeof(cmpData)<<"and cmpsize is"<<cmpSize<<std::endl;
-                // std::cout << "Compression Ratio: " << ratio<<" Amg iter: "<<c_info.amg_iter<<" Amg level: "<<c_info.amg_level<<" Rank:"<<rank<<" Datasize: "<<datasize<<" Operation: "<<c_info.op<< std::endl;
+                // std::cout<<"size of cmpdata is"<<sizeof(cmpData)<<"and cmpsize is"<<cmpSize<<std::endl;
+                if(rank==0){
+                    std::ofstream out("rank0.txt");
+                    std::streambuf *coutbuf1 = std::cout.rdbuf(); // save old buf
+                    std::cout.rdbuf(out.rdbuf());                 // redirect std::cout to out.txt!
+                    std::cout << "Compression Ratio rank 0: " << ratio << " Amg iter: " << c_info.amg_iter << " Amg level: " << c_info.amg_level << " Rank:" << rank << " Datasize: " << datasize << " Operation: " << c_info.op << std::endl;
+                    std::cout.rdbuf(coutbuf1);
+                }
+                if (rank == 1)
+                {
+                    std::ofstream out("rank0.txt");
+                    std::streambuf *coutbuf2 = std::cout.rdbuf(); // save old buf
+                    std::cout.rdbuf(out.rdbuf());
+                    std::cout << "Compression Ratio Rank 1: " << ratio << " Amg iter: " << c_info.amg_iter << " Amg level: " << c_info.amg_level << " Rank:" << rank << " Datasize: " << datasize << " Operation: " << c_info.op << std::endl;
+                    std::cout.rdbuf(coutbuf2);
+                }
                 delete[] cmpData;
 
             /*
