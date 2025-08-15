32c09c7f5f965cbdbbc15aaaa1d679b4878969c7
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-15 12:55:57 -0400

    changes

--- runjob.sh
+++ runjob.sh
@@ -2,14 +2,19 @@
 #SBATCH --job-name=amg_job
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 #SBATCH --time=03:00:00
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 
 # Optional: load modules or activate environment
-module load gcc openmpi
+module load spack
+module load cuda/12.3.0
+module load gcc/12.3.0
+module load openmpi/5.0.1
+module load anaconda3
+module load cmake/3.27.9
+
 
 # Recommended: enable core dumps for debugging
 ulimit -c unlimited
