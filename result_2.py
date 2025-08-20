3e414b05d403ee5ee45f230378d42a735067849a
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-20 13:56:51 -0400

    changes

--- result_2.py
+++ result_2.py
@@ -5,11 +5,35 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 
+it=[]
+cr=[]
+op=[]
+
+def get_compression_ratio(line):
+    return 5
+
+def get_iteration(line):
+    return 5
+
+def get_level(line):
+    return 5
+
+def get_datasize(line):
+    return 5
+
+def get_op(line):
+    return "Smooth"
 
 with open("slurm-4441704.output") as fh:
     for line in fh:
         if line.startswith("Compression"):
             print(line)
+            cr=get_compression_ratio(line)
+            iter=get_iteration(line)
+            level=get_level(line)
+            size=get_datasize(line)
+            op=get_op(line)
+
 
 
 
