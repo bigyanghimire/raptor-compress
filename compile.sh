60e1b1f9f40e749a0532558e2181cfcefff1f359
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-08-29 12:57:50 -0400

    changes

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@ $mode=$1
 cmake -B build -DWITH_MPI=ON -DCLUSTER_MODE=$1 -DWITH_CUDA=$2 && \
 cmake --build build --parallel $(nproc) -- VERBOSE=1 &&
 clear && \
-#    mpirun -n 1  build/examples/example
+mpirun -n 1  build/examples/example
 # mpirun -n 1  build/examples/benchmark_amg
